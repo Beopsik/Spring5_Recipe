1903451d1f1b6bef0d2f98c47bd53a0bfdf19066
BeobSik <tjswl756@naver.com>
2022-02-12 20:21:41 +0900

    Init IoC container, Scan annotation and Get bean from IoC container

--- demoSpringCore/src/main/java/com/example/demospringcore/DemoSpringCoreApplication.java
+++ demoSpringCore/src/main/java/com/example/demospringcore/DemoSpringCoreApplication.java
@@ -2,12 +2,18 @@ package com.example.demospringcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DemoSpringCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringCoreApplication.class, args);
-    }
+//        SpringApplication.run(DemoSpringCoreApplication.class, args);
+        ApplicationContext context=new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
+        SequenceGenerator generator = context.getBean("sequenceGenerator", SequenceGenerator.class);
 
+        System.out.println(generator.getSequence());
+        System.out.println(generator.getSequence());
+    }
 }
