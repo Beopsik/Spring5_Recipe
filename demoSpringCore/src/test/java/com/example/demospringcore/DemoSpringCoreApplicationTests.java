f25ed0f751bbf6322bb19d21b46b76f62c0864eb
BeobSik <tjswl756@naver.com>
2022-02-12 20:01:55 +0900

    Init Spring Core project

---
+++ demoSpringCore/src/test/java/com/example/demospringcore/DemoSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
